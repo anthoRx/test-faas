53b8e68a09985a4444cee53c674d3c4620b9b21a
Anthony Roux <anthony.roux_ext@michelin.com>
2018-06-24 21:56:13 +0200

    Add lib path

--- costcoTest/run.py
+++ costcoTest/run.py
@@ -1,6 +1,8 @@
 import pandas as p
 import numpy as np
 from sklearn import linear_model, metrics
+import sys, os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'costcoEnv/Lib/site-packages')))
 
 
 print("It works :)")
